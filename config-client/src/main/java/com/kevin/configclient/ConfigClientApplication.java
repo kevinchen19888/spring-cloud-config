ddc1c5040a034222489193849b73c7c01ecb62fd
kevinchen19888 <kevinchen19888@163.com>
2019-04-20 23:07:39 +0800

    optimization

--- config-client/src/main/java/com/kevin/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/kevin/configclient/ConfigClientApplication.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableEurekaClient
 @EnableDiscoveryClient
-@RefreshScope
+@RefreshScope   // 动态刷新配置,使用时在需要的类上进行添加
 public class ConfigClientApplication {
 
     /**
      * 使用post请求方式发送以下请求,config-client会重新读取配置文件
-     * 当再次请求接口:http://localhost:8882/hi 时配置已经更新
+     * http://localhost:8881/actuator/bus-refresh  // TODO 注意8881为多实例部署时的端口号
      *
-     * http://localhost:8881/actuator/bus-refresh
+     * 当再次请求接口:http://localhost:8882/hi 时配置已经更新
      *
      * /actuator/bus-refresh接口可以指定服务，即使用"destination"参数，
      * 比如 “/actuator/bus-refresh?destination=customers:**” 即刷新服务名为customers的所有服务
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 此项目为springCloud配置中心Demo项目,所有服务基于eureka进行服务注册;  
-本地启动项目时:依次启动eureka-servr,config-server,config-client;
+本地启动项目时:依次启动eureka-server,config-server,config-client;
